b0fa1c53ccffcb78dc6f68350b76d5daf3f745e6
Jeroen Laverman <jeroen@laverman.de>
2022-06-23 19:41:58 +0200

    add select entities

--- custom_components/solarfocus/__init__.py
+++ custom_components/solarfocus/__init__.py
@@ -3,16 +3,18 @@ from __future__ import annotations
 from datetime import timedelta
 import logging
 
+
+import voluptuous as vol
+
 from homeassistant.config_entries import ConfigEntry
 from homeassistant.const import CONF_HOST, CONF_PORT, CONF_SCAN_INTERVAL, Platform
 from homeassistant.core import HomeAssistant
 from homeassistant.exceptions import ConfigEntryNotReady
+import homeassistant.helpers.config_validation as cv
 
-from homeassistant.helpers.entity import Entity, EntityDescription
-from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
-from pysolarfocus import SolarfocusAPI
 from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 
+from .coordinator import SolarfocusDataUpdateCoordinator
 from .const import DOMAIN
 
 
@@ -20,7 +22,9 @@ _LOGGER = logging.getLogger(__name__)
 
 # TODO List the platforms that you want to support.
 # For your initial PR, limit it to 1 platform.
-PLATFORMS: list[Platform] = [Platform.SENSOR]
+PLATFORMS: list[Platform] = [Platform.SENSOR, Platform.SELECT]
+
+SERVICE_ENABLE_COOLING = vol.Schema({vol.Required("cooling"): cv.boolean})
 
 
 async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
@@ -39,16 +43,27 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
     hass.data[DOMAIN][entry.entry_id] = coordinator
 
     for platform in PLATFORMS:
-        if entry.options.get(platform, True):
-            hass.async_add_job(
-                hass.config_entries.async_forward_entry_setup(entry, platform)
-            )
+        hass.async_create_task(
+            hass.config_entries.async_forward_entry_setup(entry, platform)
+        )
 
-    entry.async_on_unload(entry.add_update_listener(async_reload_entry))
-    hass.data.setdefault(DOMAIN, {})
-    hass.data[DOMAIN][entry.entry_id] = coordinator
+    # hass.data[DOMAIN][entry.entry_id] = coordinator
+    #
+    # for platform in PLATFORMS:
+    #    if entry.options.get(platform, True):
+    #        hass.async_add_job(
+    #            hass.config_entries.async_forward_entry_setup(entry, platform)
+    #        )
+    #
+    # entry.async_on_unload(entry.add_update_listener(async_reload_entry))
+    # hass.data.setdefault(DOMAIN, {})
+    # hass.data[DOMAIN][entry.entry_id] = coordinator
 
-    hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+    # hass.config_entries.async_setup_platforms(entry, PLATFORMS)
+
+    # hass.services.async_register(
+    #    DOMAIN, "enable_cooling", coordinator.update_hc1_cooling
+    # )
 
     return True
 
@@ -65,93 +80,3 @@ async def async_reload_entry(hass: HomeAssistant, entry: ConfigEntry) -> None:
     """Reload config entry."""
     await async_unload_entry(hass, entry)
     await async_setup_entry(hass, entry)
-
-
-class SolarfocusDataUpdateCoordinator(DataUpdateCoordinator):
-    """Get the latest data and update the states."""
-
-    def __init__(self, hass, modbus_client, entry):
-        """Init the Solarfocus data object."""
-
-        self.api = SolarfocusAPI(modbus_client, 1)
-        self.name = entry.title
-        self._entry = entry
-        self.hass = hass
-
-        super().__init__(
-            hass,
-            _LOGGER,
-            name=DOMAIN,
-            update_interval=timedelta(seconds=self._entry.data[CONF_SCAN_INTERVAL]),
-        )
-
-    async def _async_update_data(self):
-        """Update data via library."""
-        if not await self.hass.async_add_executor_job(self.api.update):
-            _LOGGER.debug("Data updated failed")
-        else:
-            _LOGGER.debug("Data updated successfully")
-
-
-class SolarfocusEntity(Entity):
-    """Defines a base Solarfocus entity."""
-
-    _attr_should_poll = False
-
-    def __init__(
-        self,
-        coordinator: SolarfocusDataUpdateCoordinator,
-        description: EntityDescription,
-    ) -> None:
-        """Initialize the Atag entity."""
-        self.coordinator = coordinator
-        self._name = coordinator._entry.title
-        self._state = None
-
-        self.entity_description = description
-
-    @property
-    def device_info(self) -> dict:
-        """Return info for device registry."""
-        device = self._name
-        return {
-            "identifiers": {(DOMAIN, device)},
-            "name": "Solarfocus eco manager-touch",
-            "model": "eco manager-touch",
-            "sw_version": "21.040",
-            "manufacturer": "Solarfocus",
-        }
-
-    @property
-    def state(self):
-        """Return the current state."""
-        sensor = self.entity_description.key
-        value = getattr(self.coordinator.api, sensor)
-        if isinstance(value, float):
-            try:
-                rounded_value = round(float(value), 2)
-                return rounded_value
-            except ValueError:
-                return value
-        return value
-
-    @property
-    def available(self):
-        """Return True if entity is available."""
-        return self.coordinator.last_update_success
-
-    @property
-    def unique_id(self):
-        """Return a unique ID to use for this entity."""
-        sensor = self.entity_description.key
-        return f"{self._name}_{sensor}"
-
-    async def async_added_to_hass(self):
-        """Connect to dispatcher listening for entity data notifications."""
-        self.async_on_remove(
-            self.coordinator.async_add_listener(self.async_write_ha_state)
-        )
-
-    async def async_update(self):
-        """Update entity."""
-        await self.coordinator.async_request_refresh()
---
+++ custom_components/solarfocus/coordinator.py
@@ -0,0 +1,98 @@
+"""Demo platform that offers a fake select entity."""
+
+from datetime import timedelta
+import logging
+from homeassistant.const import CONF_SCAN_INTERVAL
+from homeassistant.helpers.update_coordinator import DataUpdateCoordinator
+from pysolarfocus import SolarfocusAPI
+
+from .const import (
+    CONF_BOILER,
+    CONF_BUFFER,
+    CONF_HEATING_CIRCUIT,
+    CONF_HEATPUMP,
+    CONF_PHOTOVOLTAIC,
+    DOMAIN,
+)
+
+_LOGGER = logging.getLogger(__name__)
+
+
+class SolarfocusDataUpdateCoordinator(DataUpdateCoordinator):
+    """Get the latest data and update the states."""
+
+    def __init__(self, hass, modbus_client, entry):
+        """Init the Solarfocus data object."""
+
+        self.api = SolarfocusAPI(modbus_client, 1)
+        self.name = entry.title
+        self._entry = entry
+        self.hass = hass
+
+        super().__init__(
+            hass,
+            _LOGGER,
+            name=DOMAIN,
+            update_interval=timedelta(seconds=self._entry.data[CONF_SCAN_INTERVAL]),
+        )
+
+    async def _async_update_data(self):
+        """Update data via library."""
+
+        success = True
+
+        if self._entry.data[CONF_HEATING_CIRCUIT]:
+            success &= True and await self.hass.async_add_executor_job(
+                self.api.update_heating
+            )
+
+        if self._entry.data[CONF_BUFFER]:
+            success &= True and await self.hass.async_add_executor_job(
+                self.api.update_buffer
+            )
+
+        if self._entry.data[CONF_BOILER]:
+            success &= True and await self.hass.async_add_executor_job(
+                self.api.update_boiler
+            )
+
+        if self._entry.data[CONF_HEATPUMP]:
+            success &= True and await self.hass.async_add_executor_job(
+                self.api.update_heatpump
+            )
+
+        if self._entry.data[CONF_PHOTOVOLTAIC]:
+            success &= True and await self.hass.async_add_executor_job(
+                self.api.update_photovoltaic
+            )
+
+        if not success:
+            _LOGGER.debug("Data updated failed")
+        else:
+            _LOGGER.debug("Data updated successfully")
+
+    async def update_hp_smart_grid(self, value: str):
+        """Set Smart Grid Mode"""
+        _LOGGER.debug("update_hp_smart_grid: %s", (int(value) == 4))
+        await self._update(self.api.hp_smart_grid_request_operation, (int(value) == 4))
+        await self.hass.async_add_executor_job(self.api.update_heatpump)
+
+    async def update_hc1_cooling(self, value: str):
+        """Set Cooling"""
+        _LOGGER.debug("update_hc1_cooling: %s", bool(int(value)))
+        await self._update(self.api.hc1_enable_cooling, bool(int(value)))
+        await self.hass.async_add_executor_job(self.api.update_heating)
+
+    async def update_hc1_mode_holding(self, value: str):
+        """Set Heating Mode"""
+        _LOGGER.debug("update_hc1_mode_holding: %s", int(value))
+        await self._update(self.api.hc1_set_mode, int(value))
+        await self.hass.async_add_executor_job(self.api.update_heating)
+
+    async def _update(self, func, value):
+        if not await self.hass.async_add_executor_job(func, value):
+            _LOGGER.debug("Writing Data failed")
+        else:
+            _LOGGER.debug("Writing Data successfully")
+
+    # bo1_enable_circulation
